e45772fbe1de905cd85d41e8a308557ad711e843
markymark <smallitle@hotmail.com>
2013-08-11 22:43:06 -0700

    use mucbuc-jsbag

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 // development only
 if ('development' == app.get('env')) {
