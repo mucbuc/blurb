13cf455e789fc73d48e749ed0c91c65447a2532d
markymark <smallitle@hotmail.com>
2013-10-30 11:09:40 -0700

    basic test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 
 var assert = require( 'assert' )
+  , events = require( 'events' )
   , field = require( '../lib/field' );
 
 assert( typeof field !== 'undefined' );
 
+
+testBasic();
+
 process.on( 'exit', function() {
 	console.log( 'test passed' ); 
-
 });
+
+
+
+function testBasic() {
+	var COUNT = 1000
+	  , e = new events.EventEmitter();
+	
+	e.setMaxListeners( COUNT ); 
+
+	for (var i = 0; i < COUNT; ++i) {
+		field.addCell( e );
+		assert( field.countCells() == i + 1 );
+	}
+}
